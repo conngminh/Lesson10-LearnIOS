1a372808925919035d570ca1e7d234f628b58aeb
conngminh <congminhhttt@gmail.com>
2022-01-24 10:32:06 +0700

    code basic timer

--- ViewController.swift
+++ ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    var timer: Timer? //bước 1: khởi tạo timer
+    var second = 0 // Bước 2: biến second để hẹn giờ
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLoading()
     }
 
-
+    func setupLoading() {
+        loading.startAnimating()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:  #selector(hideLoading), userInfo: nil, repeats: true) //bước 1.1: khởi tạo timer
+    }
+    
+    @objc func hideLoading() {
+        second += 1
+        if second == 10 {
+            loading.stopAnimating()
+            loading.hidesWhenStopped = true
+            //dừng timer
+            timer?.invalidate()
+            timer = nil //huỷ toàn bộ timer
+            //
+            
+        }
+        print(second)
+    }
 }
 
