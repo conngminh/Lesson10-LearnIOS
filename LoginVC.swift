ab24c2cf67af723546dd06907fa3a847ca36baad
conngminh <congminhhttt@gmail.com>
2022-02-04 15:40:20 +0700

    UIImagepicker Controller

--- LoginVC.swift
+++ LoginVC.swift
@@ -12,6 +12,8 @@ class LoginVC: UIViewController {
     @IBOutlet weak var tfUser: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
     @IBOutlet weak var imgAvatar: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupImageAvatar()
@@ -22,6 +24,31 @@ class LoginVC: UIViewController {
         imgAvatar.layer.masksToBounds = true
         imgAvatar.layer.borderWidth = 1
         imgAvatar.layer.borderColor = UIColor.darkGray.cgColor
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imgAvatar.isUserInteractionEnabled = true
+        imgAvatar.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func chooseImage() {
+        let alert = UIAlertController(title: "Please choose your avatar", message: nil, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "Camera", style: .default) { action1 in
+            //Open camera
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+
+        let action2 = UIAlertAction(title: "Photo library", style: .default) { action2 in
+            //open photo
+            self.imagePicker.sourceType = .photoLibrary
+            self.imagePicker.delegate = self
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+
+        let action3 = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addAction(action3)
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func tapOnLogin(_ sender: Any) {
@@ -74,6 +101,13 @@ class LoginVC: UIViewController {
         alert.addAction(action2)
         present(alert, animated: true, completion: nil)
     }
-    
-    
+}
+
+extension LoginVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imgAvatar.image = image //SET AVATAR = ANH DUOC CHON TRONG PHOTO LIBRARY
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
